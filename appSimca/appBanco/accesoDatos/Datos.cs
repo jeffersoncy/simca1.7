e73f9481f1a7d382131c26bdee4c887eef45936c
jeffersoncy <48133501+jeffersoncy@users.noreply.github.com>
2020-03-26 23:17:17 -0500

    app simca

---
+++ appSimca/appBanco/accesoDatos/Datos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Oracle.DataAccess.Client;
+namespace appBanco.accesoDatos
+{
+    class Datos
+    {
+        //construyo la cadena de conexión desde localhost copiando los datos 
+        //el arroba hace que no omita las cadenas con escape
+        string cadenaConexion = @"Data Source=localhost;User ID=simca;Password=oracle";
+
+        //metodo para ejecutar DML(insert, update, delete)
+        public int ejecutarDML(string consulta)
+        {
+            int filasAfectadas;
+            //paso 1: creo una conexion
+            OracleConnection miConexion = new OracleConnection(cadenaConexion);
+            //paso 2: creo un comando 
+            OracleCommand comando = new OracleCommand(consulta, miConexion);
+            //paso 3: abrir conexion
+            miConexion.Open();
+            /*paso 4: ejecuto el comando. Este devuelve el numero de filas 
+             * que se afectaron ya sea con el insert, update o delete que viene en
+             * la consulta */
+            filasAfectadas = comando.ExecuteNonQuery();
+            return filasAfectadas;
+        }
+        //metodo para ejecutar sentencias select
+        public DataSet ejecutarSELECT(string consulta)
+        {
+            //Paso 1: crear un data set vacio
+            DataSet ds = new DataSet();
+            //Paso 2: creo un adaptador
+            OracleDataAdapter miAdaptador = new OracleDataAdapter(consulta, cadenaConexion);
+            //Paso 3: lleno el data set a través del adaptador
+            miAdaptador.Fill(ds, "ResultadoDatos");
+            return ds;
+        }
+    }
+}
---
+++ appSimca/appBanco/logica/departamento.cs
@@ -0,0 +1,54 @@
+﻿//hello world
+//buenas a todo mundo menos a sebastian
+using appBanco.accesoDatos;
+using System;
+using System.Data;
+
+namespace appBanco.logica
+
+
+{ 
+    class departamento
+    {
+
+        Datos misDatos = new Datos();
+        public int registrarDepartamento(int departamento_codigo, int facultad_codigo, String departamento_nombre,
+            String departamento_aniofundacion)
+        {
+            string consulta;
+            // profesor_codigo
+            //consulta = "insert into departamento values (6,4,'departamento','12/02/1990')";
+            consulta = "insert into departamento values ("+departamento_codigo+","+facultad_codigo+",'"+departamento_nombre+"','"+departamento_aniofundacion+"')";
+            // ejecutar insert update delete DML
+            return misDatos.ejecutarDML(consulta);
+        }
+
+
+        public int actualizar_Departamento(int departamento_codigo,  int facultad_codigo, String departamento_nombre, String departamento_aniofundacion)
+        {
+            string consulta;
+
+            consulta = "update departamento set facultad_codigo = "+facultad_codigo+", departamento_nombre = '"+departamento_nombre+
+                "', departamento_aniofundacion = '"+departamento_aniofundacion+"' where departamento_codigo = "+departamento_codigo;
+            // ejecutar insert update delete DML
+            return misDatos.ejecutarDML(consulta);
+        }
+
+
+        public DataSet mostrarDepartamentos()
+        {
+            string consulta;
+            consulta = "select departamento_codigo as codigo,  departamento_nombre as nombre from departamento";
+           return misDatos.ejecutarSELECT(consulta);
+        }
+
+        public DataSet mostrarInfoDepartamentos()
+        {
+            string consulta;
+            consulta = "select departamento_codigo as codigo, facultad_codigo, departamento_nombre as nombre, departamento_aniofundacion as año from departamento";
+            return misDatos.ejecutarSELECT(consulta);
+        }
+
+
+    }
+}
